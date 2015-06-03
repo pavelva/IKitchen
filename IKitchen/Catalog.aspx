af6f698712a70689b7c3814999d50251f0fee87a
pavel <pavelva@hotmail.com>
2015-06-03 21:50:37 +0300

    catalog

--- IKitchen/Catalog.aspx
+++ IKitchen/Catalog.aspx
@@ -3,7 +3,11 @@
     <link rel="stylesheet" type="text/css" href="Style/Catalog.css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
+    <h2>קטלוג מוצרים</h2>
+    <div id="itemTypePicker" class="itemTypePicker" runat="server">
+        <h3>בחר מוצא:</h3>
+    </div>
+    
     <asp:ListView runat="server" DataSourceID="CatalogDataSource">
         <LayoutTemplate>
             <div runat="server" id="itemPlaceHolder"></div>
@@ -20,10 +24,13 @@
         </LayoutTemplate>
         <ItemTemplate>
             <div class="catalogItem">
-                <span class="productType" >דגם: <%#Eval("product_model") %></span>
-                <span class="productModel" >דגם: <%#Eval("product_model") %></span>
-
-                <asp:Button Text="Buy" runat="server" />
+                <h3>
+                    <span class="productType" ><%#Eval("app_name") %></span><br />
+                    <span class="productModel" > <%#Eval("product_model") %></span>
+                    <span class="productCompany" ><%#Eval("company_name") %></span>
+                </h3>
+                <span class="productImg" > <img src="Images/Big/<%#Eval("product_model") %>.jpg" /></span>
+                <asp:Button Text="הוסף לעגלה" runat="server" />
             </div>
             
         </ItemTemplate>
--- IKitchen/Catalog.aspx.cs
+++ IKitchen/Catalog.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,15 +12,76 @@ namespace IKitchen
 {
     public partial class Catalog : System.Web.UI.Page
     {
+        int a = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-
             string sqlCatalog = "select product_id, product_model, product_price, product_install_price, product_desc, product_company, app_name, appType_name, company_name " +
                                 "from ((products Join applience on product_type = app_id) Join applience_types on product_type2 = appType_id) Join companys on product_company = company_id " +
                                 " order by company_name";
 
-            CatalogDataSource.SelectCommand = sqlCatalog;
-            CatalogDataSource.DataBind();
+            //FillCatalog(sqlCatalog);
+            //if(!IsPostBack)
+            FillProductTypes();
         }
+
+        private void FillCatalog(string sql)
+        {
+            CatalogDataSource.SelectCommand = sql;
+            CatalogDataSource.DataBind();
+            
+        }
+
+        private void FillProductTypes()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["IKitchenDB"].ConnectionString);
+            string sql = "select * from applience";
+            con.Open();
+            SqlCommand command = new SqlCommand(sql, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            DataTable comp = new DataTable();
+            adapter.Fill(comp);
+
+            foreach (DataRow r in comp.Rows)
+            {
+                int value = int.Parse(r[0].ToString());
+                string text = r[1].ToString();
+
+                CheckBox c = new CheckBox();
+                c.ID = "chk_" + value;
+                c.CssClass = "catalogChk";
+                c.Text = text;
+                c.AutoPostBack = true;
+                c.CheckedChanged += new EventHandler(Product_CheckedChanged);
+                itemTypePicker.Controls.Add(c);
+            }
+        }
+
+        protected void Product_CheckedChanged(object sender, EventArgs e)
+        {
+            List<string> products = new List<string>();
+
+            foreach (Control c in itemTypePicker.Controls)
+            {
+                if (c.GetType().Equals(typeof(CheckBox)))
+                {
+                    if(((CheckBox)c).Checked)
+                    {
+                        //string id = c.ID.Substring(c.ID.IndexOf("chk_") + 1);
+                        string name = ((CheckBox)c).Text;
+                        products.Add(name);
+                    }
+                        
+                }
+            }
+
+            string sql = "select product_id, product_model, product_price, product_install_price, product_desc, product_company, app_name, appType_name, company_name " +
+                                "from ((products Join applience on product_type = app_id) Join applience_types on product_type2 = appType_id) Join companys on product_company = company_id " +
+                                "Where app_name in ('" + string.Join("','", products) + "') " + 
+                                " order by company_name";
+            a++;
+            FillCatalog(sql);
+            
+        } 
     }
 }
